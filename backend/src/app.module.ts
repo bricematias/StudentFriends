b57cc490f27f55e6f536886d9594c018795981a5
Nebneb <bricematias3@gmail.com>
2024-11-26 23:12:46 +0100

    fix dockerfile

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
-import { MongooseModule } from "@nestjs/mongoose";
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-@Module({
-  imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URL),
-],
-=======
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
->>>>>>> eb93e0b9cebc8598a1a7d8a9ab37df64ca23ff1a
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as mongoose from "mongoose";
-
 
 async function bootstrap() {
-  await mongoose.connect(process.env.MONGODB_URL);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
